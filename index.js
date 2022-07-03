b4cfa479cd7d5beae4a6eb170141ab87c70cc966
Rabah <rmerzoug471@gmail.com>
2022-07-03 15:50:52 -0500

    commit tree 73b427da9f8e612ab0befd258c8a57fe7f500715

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+function submitdata(name, email) {
+    return fetch (" http://localhost:3000/users", {
+        method: "post",
+        headers: {
+            "Content-Type": "apolication/json",
+            "accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            Email: email,
+        }),
+    });
+
+}
+submitdata(rabah, merzoug);
